d846ebf2222f8ec50535853b719c22fccba5f2c2
trueosiris <tim@chaubet.be>
2023-02-16 14:59:15 +0100

    apache django

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 mkdir -p /var/log/apache2 2>/dev/null
 apache2ctl start
+if [ ! -f /var/www/html/index.php ]; then
+	echo "<?php echo 'test'; ?>" > /var/www/html/index.php
+fi
+if [ ! -f /var/www/index.php ]; then
+        echo "<?php echo 'test'; ?>" > /var/www/index.php
+fi
 tail -f /dev/null
