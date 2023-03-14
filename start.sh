60867afe01126a85224e9de711830afb0010d5ce
trueosiris <tim@chaubet.be>
2023-03-14 15:07:03 +0100

    added virtualenvwq

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 mkdir -p /var/log/apache2 2>/dev/null
+pip install virtualenv 2>/dev/null
 apache2ctl start
 if [ ! -f /var/www/html/index.php ]; then
 	echo "<?php echo 'test'; ?>" > /var/www/html/index.php
 fi
 if [ ! -f /var/www/index.php ]; then
-        echo "<?php echo 'test'; ?>" > /var/www/index.php
+        echo "<?php echo 'test2'; ?>" > /var/www/index.php
 fi
 tail -f /dev/null
