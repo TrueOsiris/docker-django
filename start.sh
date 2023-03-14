f376213837f4fc900e9f82bc76723a1460707baa
trueosiris <tim@chaubet.be>
2023-03-14 15:08:01 +0100

    added virtualenv

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 mkdir -p /var/log/apache2 2>/dev/null
-pip install virtualenv 2>/dev/null
 apache2ctl start
 if [ ! -f /var/www/html/index.php ]; then
 	echo "<?php echo 'test'; ?>" > /var/www/html/index.php
